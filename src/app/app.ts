58158c214741d3e5bde492f91ab5b12d3706c76d
Jake Poshepny <jposhepny99@gmail.com>
2025-12-12 00:18:57 -0600

    Panel Test

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Game} from './components/game/game';
 import {ThemeSelector} from './components/theme-selector/theme-selector';
+import {Panel} from "./components/panel/panel";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Game, ThemeSelector],
+  imports: [RouterOutlet, Game, ThemeSelector, Panel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
